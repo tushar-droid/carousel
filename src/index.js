f0b2c169c37062509728e8984c35c98e61ec6d79
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-10-02 00:13:18 -0700

    Added the dots All working properly

--- src/index.js
+++ src/index.js
@@ -6,22 +6,48 @@ var nextBtn = document.querySelector('.carousel-right-btn');
 var prevBtn = document.querySelector('.carousel-left-btn');
 var counter = 0;
 
-
+//changs the image and container size based on the window size
 window.onresize = setsizes;
 
 nextBtn.addEventListener('click', () =>{
-    if(counter>=images.length-1) return;
+    
+    if(counter>=images.length-1){
+        counter= 0;
+        images_track.style.transform = `translateX(-${image_size*counter}px)`;
+        var active_dot = document.querySelector('.active-carousel-dot');
+        active_dot.classList.remove('active-carousel-dot');
+        var next_dot = document.querySelector('.dot');
+        next_dot.classList.add('active-carousel-dot');
+        return;
+    }
     counter= counter+1
     images_track.style.transform = `translateX(-${image_size*counter}px)`
-    console.log(image_size*counter);
+    var active_dot = document.querySelector('.active-carousel-dot');
+    active_dot.classList.remove('active-carousel-dot');
+    var active_dot = active_dot.parentElement;
+    var next_dot = active_dot.nextElementSibling;
+    next_dot =next_dot.firstChild
+    next_dot.classList.add('active-carousel-dot');
 })
 
 
 prevBtn.addEventListener('click', () =>{
-    if(counter ==0) return;
+    if(counter ==0) {
+        counter = images.length-1;
+        images_track.style.transform = `translateX(-${image_size*counter}px)`;
+        var active_dot = document.querySelector('.active-carousel-dot');
+        active_dot.classList.remove('active-carousel-dot');
+        var prev_dot = document.querySelectorAll('.dot');
+        prev_dot[prev_dot.length-1].classList.add('active-carousel-dot');
+        return;}
     counter = counter-1;
-    images_track.style.transform = `translateX(-${image_size*counter}px)`
-    console.log(image_size*counter);
+    images_track.style.transform = `translateX(-${image_size*counter}px)`;
+    var active_dot = document.querySelector('.active-carousel-dot');
+    active_dot.classList.remove('active-carousel-dot');
+    var active_dot = active_dot.parentElement;
+    var prev_dot = active_dot.previousElementSibling;
+    prev_dot =prev_dot.firstChild
+    prev_dot.classList.add('active-carousel-dot');
 })
 
 function setsizes(){
@@ -31,7 +57,6 @@ function setsizes(){
      image_size = images_track.clientWidth;
      nextBtn = document.querySelector('.carousel-right-btn');
      prevBtn = document.querySelector('.carousel-left-btn'); 
-
      images.forEach(image =>{
         image.style.width = `${image_size}px;`;
         images_track.style.transform = `translateX(-${image_size*counter}px)`
